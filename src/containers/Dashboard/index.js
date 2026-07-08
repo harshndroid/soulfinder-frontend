44ac0dcee9e4e9ced947f40f67da2f9272af369c
harshndroid <harshandgit@gmail.com>
2026-07-08 18:34:24 +0530

    push

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -18,55 +18,60 @@ const Dashboard = () => {
   const [coords, setCoords] = useState({});
   const [nearbyTravellers, setNearbyTravellers] = useState([]);
   const [showUpdateUserModal, setShowUpdateUserModal] = useState(false);
-
+  console.log('here');
   const user = LocalStorageService.getItem(StorageConstants.USER);
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCoords({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-        const requestPayload = {
-          id: user?.userId,
-          location: {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCoords({
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
-          },
-          lastSeenAt: Date.now(),
-        };
+          });
+          const requestPayload = {
+            id: user?.userId,
+            location: {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            },
+            lastSeenAt: Date.now(),
+          };
 
-        ApiService.fetchApi(ApiConstants.INIT, 'POST', requestPayload)
-          .then((res) => {
-            if (res.status === 200 || res.status === 201) return res.json();
-            else if (res.status === 401 || res.status === 404) {
-              LocalStorageService.removeItem(StorageConstants.USER);
-              navigate('/login');
-              throw new Error('Api err');
-            }
-          })
-          .then((data) => {
-            console.log('Init api response', data);
-            let updatedUserData = data.data;
-            delete updatedUserData['_id'];
-            delete updatedUserData['__v'];
-            LocalStorageService.setItem(StorageConstants.USER, {
-              ...user,
-              ...updatedUserData,
-            });
-            setPhoto(data.data.photoUrl);
-            fetchNearbyTravellers(
-              {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-              },
-              user?.userId,
-              setNearbyTravellers
-            );
-          })
-          .catch((e) => console.log('Init api error:', e));
-      });
+          ApiService.fetchApi(ApiConstants.INIT, 'POST', requestPayload)
+            .then((res) => {
+              if (res.status === 200 || res.status === 201) return res.json();
+              else if (res.status === 401 || res.status === 404) {
+                LocalStorageService.removeItem(StorageConstants.USER);
+                navigate('/login');
+                throw new Error('Api err');
+              }
+            })
+            .then((data) => {
+              console.log('Init api response', data);
+              let updatedUserData = data.data;
+              delete updatedUserData['_id'];
+              delete updatedUserData['__v'];
+              LocalStorageService.setItem(StorageConstants.USER, {
+                ...user,
+                ...updatedUserData,
+              });
+              setPhoto(data.data.photoUrl);
+              fetchNearbyTravellers(
+                {
+                  latitude: position.coords.latitude,
+                  longitude: position.coords.longitude,
+                },
+                user?.userId,
+                setNearbyTravellers
+              );
+            })
+            .catch((e) => console.log('Init api error:', e));
+        },
+        (error) => {
+          console.log('geolocation error:', error);
+        }
+      );
     }
   }, []);
 
